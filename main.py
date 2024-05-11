b53ffcba712af650a9c105fbdb099242c8dcd8c5
Mana <86923626+Mana42138@users.noreply.github.com>
2024-05-11 19:32:11 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/post_data', methods=['POST'])
+def post_data():
+  webhook_url = request.args.get('webhook')
+
+  data = request.get_json()
+  print(webhook_url)
+  requests.post(webhook_url, json=data)
+  return jsonify({'message': 'Data received successfully'})
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
